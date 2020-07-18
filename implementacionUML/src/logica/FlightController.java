de0bdc811ed18101a4a094606cd29ba7504be5ad
nicocadq <nmachado.camejo@gmail.com>
2020-07-18 15:12:59 -0300

    Add methods signature for FlightController

--- implementacionUML/src/logica/FlightController.java
+++ implementacionUML/src/logica/FlightController.java
@@ -5,52 +5,21 @@ import java.util.ArrayList;
 import logica.FlightSeat;
 
 public class FlightController {
-	private ArrayList<Airport> airports = new ArrayList<Airport>();
-	private ArrayList<Flight> flights = new ArrayList<Flight>();
-	private	ArrayList<Airplane> airplanes = new ArrayList<Airplane>();
 
-	public FlightController(ArrayList<Airport> airports, ArrayList<Flight> flights, ArrayList<Airplane> airplanes) {
-		super();
-		this.airports = airports;
-		this.flights = flights;
-		airplanes = airplanes;
+	public int newFlight(Airport a1, Airport a2, LocalDate date) {
+		
 	}
 	
-	public ArrayList<Airport> getAirports() {
-		return airports;
-	}
-
-	public void setAirports(ArrayList<Airport> airports) {
-		this.airports = airports;
-	}
-
-	public ArrayList<Flight> getFlights() {
-		return flights;
-	}
-
-	public void setFlights(ArrayList<Flight> flights) {
-		this.flights = flights;
-	}
-
-	public ArrayList<Airplane> getAirplanes() {
-		return airplanes;
-	}
-
-	public void setAirplanes(ArrayList<Airplane> airplanes) {
-		airplanes = airplanes;
-	}
-
 	public void assignFlight(int flightCode, String enrolment) {
-
+		
 	}
-
-	public ArrayList<DataFlight> checkFlight(String ap1, String ap2, LocalDate date) {
-		return null;
-
+	
+	public ArrayList<DataFlight> checkFlight(Airport ap1, Airport ap2, LocalDate date){
+		
 	}
-
+	
 	public FlightSeat getFlightSeat(int flightCode, int seat) {
-		return null; //tiene que retornar en un futuro una instancia de FlightSeat
-
+		
 	}
+	
 }
