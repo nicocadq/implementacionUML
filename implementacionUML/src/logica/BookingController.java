069b658ffe13db43d0a4bd7b3a0888a1fdbc861f
nicocadq <nmachado.camejo@gmail.com>
2020-07-18 15:03:57 -0300

    Add methods signature for BookingController

--- implementacionUML/src/logica/BookingController.java
+++ implementacionUML/src/logica/BookingController.java
@@ -2,7 +2,31 @@ package logica;
 
 public class BookingController {
 	
+	public void startBooking(String name) {
+		
+	}
 	
+	public ArrayList<FlightData> checkFlight(Airport ap1, Airport ap2, LocalDate date){
+		
+	}
 	
+	public void bookChair(int flightCode, int chair) {
+		
+	}
 	
+	public float checkPrice() {
+		
+	}
+	
+	public float buyTicketByCash() {
+		
+	}
+	
+	public float buyTicketByCard(int cardNum, int amountPayments) {
+		
+	}
+	
+	public void cancelBooking() {
+		
+	}
 }
