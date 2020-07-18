c8c9ca437136e742afcd4db05f461fb30e8a1cbd
nicocadq <nmachado.camejo@gmail.com>
2020-07-18 15:12:59 -0300

    Add methods signature for FlightController

--- implementacionUML/src/logica/FlightController.java
+++ implementacionUML/src/logica/FlightController.java
@@ -1,5 +1,21 @@
 package logica;
 
 public class FlightController {
-
+	
+	public int newFlight(Airport a1, Airport a2, LocalDate date) {
+		
+	}
+	
+	public void assignFlight(int flightCode, String enrolment) {
+		
+	}
+	
+	public ArrayList<FlightData> checkFlight(Airport ap1, Airport ap2, LocalDate date){
+		
+	}
+	
+	public FlightChair getFlightChair(int flightCode, int chair) {
+		
+	}
+	
 }
