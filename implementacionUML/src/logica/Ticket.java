cf18ce0d910c091e88413470979b0b052ea99149
nicocadq <nmachado.camejo@gmail.com>
2020-07-22 00:55:02 -0300

    Fixing wrong relationship in Ticket

--- implementacionUML/src/logica/Ticket.java
+++ implementacionUML/src/logica/Ticket.java
@@ -6,22 +6,12 @@ public class Ticket {
 	
 	private LocalDate date;
 	
-	private Person person;
-	
 	public Ticket() {
 		this.date = LocalDate.now();
 	}
 	
 	public void addFlightSeat(FlightSeat fs) {
 		
-	}
-	
-	public void setPerson(Person person) {
-		this.person = person;
-	}
-	
-	public Person getPerson() {
-		return this.person;
 	}
 	
 	public float getPrice() {
